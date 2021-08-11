c7eb98af177db187ae70d233c3fd773847a820a7
live-jh <livejh7129@gmail.com>
2021-08-11 23:59:53 +0900

    add factorial

--- this-is-coding-test/search/factorial.py
+++ this-is-coding-test/search/factorial.py
@@ -1,14 +1,15 @@
 def factorial_iterative(n):
     result = 1
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         result *= i
     return result
 
 
 def factorial_recursive(n):
-    result =1
-    return n
+    if n <= 1:
+        return 1
+    return n * factorial_iterative(n - 1)
 
 
 print("반복문을 활용한 입력한 숫자까지의 곱의 합", factorial_iterative(5))
-# print("반복문을 활용한 입력한 숫자까지의 곱의 합", factorial_recursive(5))
+print("반복문을 활용한 입력한 숫자까지의 곱의 합", factorial_recursive(5))
